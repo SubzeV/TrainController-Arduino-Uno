f10037c388e36f946481aa96b41adee0fed2c816
Subzero Hiro <subzero.hiro@outlook.com>
2022-06-13 18:51:41 +0200

    Added controller & potentiometer reader
    Added the first controller (for Processing) & potentiometer reader (for Arduino Uno)

---
+++ ControllerV1TGV/ControllerV1TGV.pde
@@ -0,0 +1,91 @@
+import processing.serial.*;
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
+import javax.swing.KeyStroke;
+Serial MyPort;                                
+String KeyString = "";
+void setup()
+{
+  System.out.println("Hi");
+  
+  try // wait for 5 seconds before starting to register input.
+  {
+    Thread.sleep(5000);
+  }
+  catch(InterruptedException ex)
+  {
+    Thread.currentThread().interrupt();
+  }
+  System.out.println("Outputting now!");
+  
+  size(700, 500);
+  MyPort = new Serial(this, "COM3", 9600); // Arduino is on COM3. Enter the COM on which your Arduino is on.
+  MyPort.bufferUntil('\n');
+  
+
+  
+}
+void draw(){//Not really necessary
+  background(0, 0, 0);
+  fill(255, 0, 0);
+  text("Start using the controller...", 10, 10);
+}
+void serialEvent(Serial MyPort)throws Exception {
+   KeyString = MyPort.readStringUntil('\n');
+   KeyString = KeyString.substring(0, KeyString.indexOf(':')); // the string is split. the whole string leaving the colon is taken
+   System.out.println(KeyString); // prints the serial string for debugging purpose
+   Robot Arduino = new Robot(); // constructor of robot class
+   switch(KeyString){
+     case "Forward1" :
+       Arduino.keyPress(KeyEvent.VK_D); // presses D key.
+         try
+         {
+           Thread.sleep(100); // wait set amount of time (ms)
+         }
+         catch(InterruptedException ex)
+         {
+           Thread.currentThread().interrupt();
+         }
+       Arduino.keyRelease(KeyEvent.VK_D); // releases D key
+       break;
+     case "Back1" :
+       Arduino.keyPress(KeyEvent.VK_A); // presses A key
+         try
+         {
+           Thread.sleep(100); // wait set amount of time (ms)
+         }
+         catch(InterruptedException ex)
+         {
+           Thread.currentThread().interrupt();
+         }
+       Arduino.keyRelease(KeyEvent.VK_A); // releases A key
+       break;
+     case "Forward2" :
+       Arduino.keyPress(KeyEvent.VK_QUOTE); // presses quote key.
+         try
+         {
+           Thread.sleep(100); // wait set amount of time (ms)
+         }
+         catch(InterruptedException ex)
+         {
+           Thread.currentThread().interrupt();
+         }
+       Arduino.keyRelease(KeyEvent.VK_QUOTE); // releases quote key
+       break;
+     case "Back2" :
+       Arduino.keyPress(KeyEvent.VK_SEMICOLON); // presses semicolon key.
+         try
+         {
+           Thread.sleep(100); // wait set amount of time (ms)
+         }
+         catch(InterruptedException ex)
+         {
+           Thread.currentThread().interrupt();
+         }
+       Arduino.keyRelease(KeyEvent.VK_SEMICOLON); // releases semicolon key
+       break;
+   }
+   
+}
---
+++ PotentiometerReaderV1/PotentiometerReaderV1.ino
@@ -0,0 +1,87 @@
+// Please ignore the commented out lines that do nothing, I am still working a lot on this file.
+
+float RT = 10000;// the rated potentiometer value 10k
+float R;// the unknown resistance value 
+float R2;// the unknown resistance value R
+
+int pot1Pin = A0; // potentiometer is connected to analog 0 pin
+int pot1Value; // variable used to store the value coming from the sensor
+int percent1; // variable used to store the percentage value
+int clickedPercent1;
+int prevClickedPercent1 = 0;
+
+int pot2Pin = A1; // potentiometer is connected to analog 0 pin
+int pot2Value; // variable used to store the value coming from the sensor
+int percent2; // variable used to store the percentage value
+int clickedPercent2;
+int prevClickedPercent2 = 0;
+
+int prevPot1Val = 0;
+int prevPot2Val = 0;
+
+int throttleClicks = 17; // define the amount of position a train's throttle or combined throttle/brake lever has
+// The TGV Euroduplex for example needs 17 (8 in each direction for throttle + 0 throttle)
+int brakeClicks = 4; // a bit more complex but similar to the 'throttleClicks'.
+// For the TGV Euroduplex it is different then most locomotives as it acts as an apply or release and not a specific amount.
+
+void setup() {
+Serial.begin(9600); // initialize the serial communication
+}
+void loop() {
+
+
+// ---Pot 1---
+pot1Value = analogRead(pot1Pin); // get a reading from the potentiometer, assign the name potValue
+percent1 = map(pot1Value, 0, 1020, 0, 100); // convert reading to a percentage
+clickedPercent1 = map(pot1Value, 0, 1020, 0, throttleClicks);
+
+if (clickedPercent1 < prevClickedPercent1) {
+  Serial.println("Forward1:");
+//  Serial.println(clickedPercent1); // debug info only
+//  Serial.println(prevClickedPercent1); // debug info only
+  delay(1);
+  prevClickedPercent1 = clickedPercent1;
+  delay(1);
+} else if (clickedPercent1 > prevClickedPercent1) {
+  Serial.println("Back1:");
+//  Serial.println(clickedPercent1); // debug info only
+//  Serial.println(prevClickedPercent1); // debug info only
+  delay(1);
+  prevClickedPercent1 = clickedPercent1;
+  delay(1);
+} else {
+  delay(1);
+  prevClickedPercent1 = clickedPercent1;
+  delay(1);
+}
+
+//    Serial.println("Back1:"); // ignore, temporary code
+//    Serial.println("Forward1:"); // ignore, temporary code
+
+
+// ---Pot 2---
+pot2Value = analogRead(pot2Pin); // get a reading from the potentiometer, assign the name potValue
+percent2 = map(pot2Value, 0, 1020, 0, 100); // convert reading to a percentage
+clickedPercent2 = map(pot2Value, 0, 1020, 0, brakeClicks);
+
+if (clickedPercent2 < prevClickedPercent2) {
+  Serial.println("Forward2:");
+//  Serial.println(clickedPercent2); // debug info only
+//  Serial.println(prevClickedPercent2); // debug info only
+  delay(1);
+  prevClickedPercent2 = clickedPercent2;
+  delay(1);
+} else if (clickedPercent2 > prevClickedPercent2) {
+  Serial.println("Back2:");
+//  Serial.println(clickedPercent2); // debug info only
+//  Serial.println(prevClickedPercent2); // debug info only
+  delay(1);
+  prevClickedPercent2 = clickedPercent2;
+  delay(1);
+} else {
+  delay(1);
+  prevClickedPercent2 = clickedPercent2;
+  delay(1);
+}
+
+}
